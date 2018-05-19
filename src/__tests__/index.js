148a00f01ed5c9b76e6647eb92196f7c5c222451
Dean Tambling <dean.tambling@gmail.com>
2018-05-18 21:41:45 -0700

    Development environment set up

---
+++ src/__tests__/index.js
@@ -0,0 +1,4 @@
+import applyMinimumHeading from "..";
+
+test("applyMinimumHeading exists and is a function", () =>
+  expect(applyMinimumHeading).toBeInstanceOf(Function));
---
+++ src/index.js
@@ -0,0 +1,3 @@
+const applyMinimumHeader = () => ({});
+
+export default applyMinimumHeader;
